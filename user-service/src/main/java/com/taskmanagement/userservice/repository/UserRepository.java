db75d063b21d11a30fec90939784b745ced422f0
Oleg Orlovsky <olejatorqq@gmail.com>
2024-07-13 23:50:29 +0300

    Added ms user-service

---
+++ user-service/src/main/java/com/taskmanagement/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.taskmanagement.userservice.repository;
+
+import com.taskmanagement.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
