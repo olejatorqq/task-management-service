34865b2d7e352474fccccd65f93d509e59efc556
Oleg Orlovsky <olejatorqq@gmail.com>
2024-07-13 23:46:07 +0300

    Added ms task-service

---
+++ task-service/src/main/java/com/taskmanagement/taskservice/service/TaskService.java
@@ -0,0 +1,42 @@
+package com.taskmanagement.taskservice.service;
+
+import com.taskmanagement.taskservice.model.Task;
+import com.taskmanagement.taskservice.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task task) {
+        Task existingTask = taskRepository.findById(id).orElse(null);
+        if (existingTask != null) {
+            existingTask.setTitle(task.getTitle());
+            existingTask.setDescription(task.getDescription());
+            existingTask.setCompleted(task.isCompleted());
+            return taskRepository.save(existingTask);
+        }
+        return null;
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
